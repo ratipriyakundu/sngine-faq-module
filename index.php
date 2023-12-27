e10fcc445846259247c935509025ced1130e996d
ratipriyakundu <ratipriyakundu6@gmail.com>
2023-12-27 17:52:10 +0530

    Github action created, 27th Dec, 2023, 05:52

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once __DIR__.'/vendor/autoload.php';
+
 use Ratipriya\SngineFAQ\SngineFAQ;
 
 $faq = new SngineFAQ("Hi");
